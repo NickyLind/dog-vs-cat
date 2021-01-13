e3021ea247fb96b11dae14bb5b9a324b5916608d
Nick Lindau <nicholaithegreat@gmail.com>
2021-01-12 16:20:01 -0800

    add buttons for cat/dog and lines for them to speak

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  
+  $("button#cat-talk").click(function()  {
+    $("ul#cat").prepend("<li>~mrawwwwwant to go catch mice?</li>");
+    $("ul#dog").prepend("<li>~growwwwllike you even have to ask!");
+  });
+
+  $("button#dog-talk").click(function()  {
+    $("ul#dog").prepend("<li>Woof woof woof</li>");
+    $("ul#cat").prepend("<li>Mee-yoww</li>");
+  });
 });
